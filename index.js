d8eb7417249201646e4dbd5ec0b85ad898dbbc1e
Mirko <m.minni.1764@vallauri.edu>
2024-03-09 13:22:40 +0100

    connessione db mongo

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
+const {MongoClient} = require('mongodb');
 const express = require('express');
 const env = require('dotenv').config(); 
 const app = express();
 const port = 3000;
 
-app.post('/', (req, res) => {
+const client = new MongoClient(process.env.STRING_CONNECTION);
 
-    //chiamata al db
-  res.send('Risultato')
-})
 
-app.listen(port, () => {
-    
-    console.log(`Example app listening on port ${port}`);
-});
 
-app.post('/', (req, res) => {
-  // Your code here
-  res.send('Risultato');
-});
